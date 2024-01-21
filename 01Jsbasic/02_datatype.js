3480a80a345551070dd840cdd4b4aefa8487a591
986MukeshYadav <119566234+986MukeshYadav@users.noreply.github.com>
2024-01-21 17:38:54 +0000

    datatype

---
+++ 01Jsbasic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treats all js code as newer version
+
+//alert(3+4) // we are adding nodejs , not browser
+console.log(3
+    +5 ) //code reablity should be high
+console.log(
+    "Mukesh "
+)
+
+let name="Mukesh"
+let age="22"
+let isLoggedIn="flase"
+
+//number => 2 to power 53
+//bigint is used very hug data
+//string=>
+//boolean=true/flase
+//null=>standalone value
+//undefined
+//symbole => unique data
+//object
+
+console.log(typeof "mukesh") // string
+console.log(typeof age)    //string
+console.log(typeof null)  // Object
+console.log(typeof undefined ) // undefined
