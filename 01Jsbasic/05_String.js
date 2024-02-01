5ecdc3871edfbb1597e79f022d8e51871477590e
986MukeshYadav <119566234+986MukeshYadav@users.noreply.github.com>
2024-02-01 18:01:09 +0000

    Diferent types of String in js

---
+++ 01Jsbasic/05_String.js
@@ -0,0 +1,32 @@
+const Fname="Mukesh"
+const Lname="Yadav"
+
+//console.log(Fname + Lname + "Kumar")
+
+//console.log(`First name is ${Fname} and Last name is ${Lname}`);
+
+const name1= new String('Kumar-yadav-mukesh')
+//console.log(name1[0]);
+//console.log(name1.__proto__);
+
+//console.log(name1.length);
+//console.log(name1.toUpperCase());
+//console.log(name1.chartAt(2));
+//console.log(name1.indexOf(m));
+
+const newString = name1.substring(-3,1)
+console.log(newString);
+
+const anotherString = name1.slice(-3,1)
+console.log(anotherString);
+
+const newStringone ="   mukesh   "
+console.log(newStringone);
+console.log(newStringone.trim());
+
+const url="http://mukesh.com/kumar%20yadav"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('ayhooo'))
+
+console.log(name1.split('-'));
