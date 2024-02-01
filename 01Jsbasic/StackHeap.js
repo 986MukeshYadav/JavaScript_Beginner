759def34eae648af1a14db6b30918c1bf39ab2ca
986MukeshYadav <119566234+986MukeshYadav@users.noreply.github.com>
2024-02-01 17:20:40 +0000

    Stack and Heap memory

---
+++ 01Jsbasic/StackHeap.js
@@ -0,0 +1,25 @@
+
+//=============================================================
+//Stack(preemative DataTypes)
+//Heap(Non-Preemative DataTypes)
+
+let myemail="mky@gmaail.com";
+
+let aneamil=myemail;
+aneamil="mukesh kumar yadav"
+
+console.log(myemail);
+console.log(aneamil);
+
+//==================================================
+
+let U1={
+    username:"mukesh@email.com",
+    name:"Mukesh"
+}
+let U2 = U1
+
+U1.email="kumar@email.com";
+console.log(U1.email);
+console.log(U2.email);
+//it is short and easy  to understand
--- 01Jsbasic/datatypes_summary.js
+++ 01Jsbasic/datatypes_summary.js
@@ -49,3 +49,4 @@ Object - object
 */
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
