e65f528a50d816f22f40c0784d5406228b99ed93
986MukeshYadav <119566234+986MukeshYadav@users.noreply.github.com>
2024-02-02 17:57:30 +0000

    numbers and maths

---
+++ 01Jsbasic/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score=400
+// console.log(score);
+
+const ac=new Number(100)
+// console.log(ac);
+
+// console.log(ac.toString().length);
+// console.log(ac.toFixed(2));
+
+const otherNumber = 34.8966
+
+// console.log(otherNumber.toPrecision(4));
+const hunderds = 10000000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+//+++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(4,5,7,9));
+// console.log(Math.min(4,5,7,9));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) +min)
+
