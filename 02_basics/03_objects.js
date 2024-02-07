18563a218cb696ede02f6befb494a9c0257af65d
986MukeshYadav <119566234+986MukeshYadav@users.noreply.github.com>
2024-02-07 18:16:09 +0000

    Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton
+//object.create
+
+//object literals
+
+const mySym = Symbol("key1")
+
+const JsUser={
+    name:"Mukesh",
+    "full name":"Mukesh Kumar Yadav",
+    [mySym]:"mykey1",
+    age:20,
+    location:"Janakpur",
+    email:"mky@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])
+
+JsUser.email="mukesh@google.com"
+//Object.freeze(JsUser)
+JsUser.email = "kumar@gmail.com"
+//console.log(JsUser);
+
+JsUser.greeting =function(){
+    // console.log("Hello Js User");
+}
+
+JsUser.greetingTwo =function(){
+    // console.log(`Hello Js User,${this.name}`);
+}
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
