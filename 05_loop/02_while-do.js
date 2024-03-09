f00c564984b63a3102bac7cb91b88d641c59910a
Mukesh <myadav986@rku.ac.in>
2024-03-10 00:00:07 +0530

    while/dowhile loop

---
+++ 05_loop/02_while-do.js
@@ -0,0 +1,22 @@
+// While-loop
+let index = 0;
+while(index<=10){
+    //console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ['a', 'b', 'c']
+let arr=0;
+while(arr < myArray.length){
+    //console.log(`Value of index is ${myArray[arr]}`);
+    arr= arr + 1;
+}
+
+//Do-While-loop
+
+let score = 11
+
+do{
+  console.log(`Value of index is ${score}`)
+  score = score + 1
+}while(score <=10);
