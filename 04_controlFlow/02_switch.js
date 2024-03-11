6f585460796eef962b3a93d4e21f973dfa479ddf
Mukesh <myadav986@rku.ac.in>
2024-03-11 22:44:45 +0530

    SwitchCase

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,70 @@
+// switch (value){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch(month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("February")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("April")
+//         break;
+//     case 5:
+//         console.log("May")
+//         break;
+//     case 6:
+//         console.log("June")
+//         break;
+//     case 7:
+//         console.log("July")
+//         break;
+//     case 8:
+//         console.log("August")
+//         break;
+//     case 9:
+//         console.log("September")
+//         break;
+//     case 10:
+//         console.log("October")
+//         break;
+//     case 11:
+//         console.log("November")
+//         break;
+//     case 12:
+//         console.log("December")
+//         break;
+//     default:
+//         console.log("Invalid Month")
+//         break;
+// }
+
+
+const month = "May"
+switch(month) {
+    case "January":
+        console.log("January")
+        break;
+    case "February":
+        console.log("February")
+        break;
+    case "March":
+        console.log("March")
+        break;
+    case "May":
+        console.log("May")
+        break;
+    default:
+        console.log("Invalid Month")
+
+}
